fce894fa103ffc8b5a0c7240b4cb42b465267ded
Ted Kesgar <t.kesgar@live.com>
2019-06-21 06:30:56 +0700

    WIP

--- src/components/CreateCharaModal/view.test.js
+++ src/components/CreateCharaModal/view.test.js
@@ -3,7 +3,9 @@ import localVue from '../../utils/test/local-vue'
 import CreateCharaModal from './view'
 
 describe('CreateCharaModal', () => {
-  const wrapper = mount(CreateCharaModal, {localVue})
+  const wrapper = mount(CreateCharaModal, {
+    localVue
+  })
 
   test('should mount successfully', () => {
     expect(wrapper.isVueInstance()).toBe(true)
